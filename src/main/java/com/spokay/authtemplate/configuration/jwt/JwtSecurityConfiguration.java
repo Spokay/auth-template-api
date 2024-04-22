c71d559709cec2e8d92a577c2bada9680878b4c8
Spokay <hugobressange@gmail.com>
2024-04-22 10:39:15 +0200

    feat: open api documentation implemented

--- src/main/java/com/spokay/authtemplate/configuration/jwt/JwtSecurityConfiguration.java
+++ src/main/java/com/spokay/authtemplate/configuration/jwt/JwtSecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -36,6 +37,9 @@ public class JwtSecurityConfiguration {
             .cors(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(
                     requestMatcher -> requestMatcher
+                            .requestMatchers("/api-docs/**").permitAll()
+                            .requestMatchers("/api-test/**").permitAll()
+                            .requestMatchers("/swagger-ui/**").permitAll()
                             .requestMatchers("/api/auth/**").permitAll()
                             .anyRequest().authenticated()
             )
